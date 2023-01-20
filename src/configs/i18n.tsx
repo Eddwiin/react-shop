e526c841f5b31000dfb0ccbab4f8747db2c76102
Eddwiin <maguran.mahendrarajan@gmail.com>
2023-01-20 21:38:04 +0100

    add tailwind + translate fr

---
+++ src/configs/i18n.tsx
@@ -0,0 +1,25 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
+import translationFR from './i18n/fr/translate.json';
+
+const resources = {
+    fr: {
+        translation: translationFR
+    }
+};
+
+i18n
+    .use(Backend)
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources,
+        fallbackLng: "fr",
+        interpolation: {
+            escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+        }
+    })
+
+export default i18n;
--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -1,7 +1,11 @@
+import { useTranslation } from "react-i18next"
+
 export default function Header() {
+    const { t } = useTranslation();
+
     return (
-        <div>
-            Header
+        <div className="bg-blue p-4">
+            {t('Header.AppName')}
         </div>
     )
 }

afc20e545fc1e1c3070011a9db9775053fe02313
Eddwiin <maguran.mahendrarajan@gmail.com>
2023-01-24 22:22:46 +0100

    config redux

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import './index.scss';
 import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
 
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -13,6 +12,7 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
